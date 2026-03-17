2a60740e30623404593cc7726fb0f78a46c5b846
dreireyez <djemandreif@gmail.com>
2026-03-18 05:27:09 +0800

    Updated security

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2118012515-dc75e",
-  "appId": "1:348654745497:web:aaaf8929e9b7b74fd50508",
-  "apiKey": "AIzaSyBV2d56x1NjiIwczWLwI-PJ9UUBF0NXYDo",
-  "authDomain": "studio-2118012515-dc75e.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "348654745497"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
